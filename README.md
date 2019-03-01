3b196c24cbf2b6823af4bcaed946520605617ade
fieldpalmer <gfieldpalmer@gmail.com>
2019-02-28 22:20:13 -0500

    responsive sizing and fonts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gfp-portfolio
+
+### welcome to my porftfolio's index.html - we're gonna kick it old school and stick to the basics - there's plenty of work at the links herein demonstrating my knowledge of and expereience in more modern technologies including Vue.js, MERN, SQL, and more
---
+++ typed-js/typedJS.js
@@ -0,0 +1,13 @@
+// Typed.js code
+var typed = new Typed("#typed-element", {
+  strings: [
+    "Greetings, visitor!",
+    "Welcome to my portfolio",
+    "I like to build useful tools and create fun apps",
+    "This site is meant to introduce myself and show off",
+    "Take a look around for more!"
+  ],
+  typeSpeed: 50,
+  backSpeed: 35
+  // loop: true // Default value
+});
